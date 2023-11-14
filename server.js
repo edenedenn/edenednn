8f30fcb30cc1d0e21a5bca984b8c75e5f6c4ae3c
JTtsz <144902652+JTtsz@users.noreply.github.com>
2023-11-14 19:42:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const assert = require('assert');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
-const mongourl = 'mongodb+srv://s1299841:1303313524Aa@cluster0.uuchtz5.mongodb.net/?retryWrites=true&w=majority'; 
+
+const mongourl = ''; 
 const dbName = 'test';
 
 const express = require('express');
@@ -15,8 +16,6 @@ const session = require('cookie-session');
 app.set('view engine', 'ejs');
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
 
 //2.userinfo
@@ -34,9 +33,29 @@ app.use(session({
     keys: [SECRETKEY],
 }));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const createDocument = function(db, createddocuments, callback){
+    const client = new MongoClient(mongourl);
+    client.connect(function(err) {
+        assert.equal(null, err);
+        console.log("Connected successfully to the MongoDB database server.");
+        const db = client.db(dbName);
+
+        db.collection('Library_document_info').insertOne(createddocuments, function(error, results){
+            if(error){
+            	throw error
+            };
+            console.log(results);
+            return callback();
+        });
+    });
+}
+
 
 const findDocument =  function(db, criteria, callback){
-    let cursor = db.collection('book_information').find(criteria);
+    let cursor = db.collection('Library_document_info').find(criteria);
     console.log(`findDocument: ${JSON.stringify(criteria)}`);
     cursor.toArray(function(err, docs){
         assert.equal(err, null);
@@ -86,9 +105,64 @@ app.get('/logout', function(req, res){
     res.redirect('/login');
 });
 
+app.get('/list', function(req, res){
+    console.log("Show all information! ");
+    handle_Find(res,req.query.docs);
+    
+});
+
 app.get('/home', function(req, res){
     console.log("...Welcome to the home page!");
     return res.status(200).render("home");
+});
+
+app.get('/create', function(req, res){
+    return res.status(200).render("create");
+});
+
+app.post('/create', function(req, res){
+    const client = new MongoClient(mongourl);
+    client.connect(function(err){
+        assert.equal(null, err);
+        console.log("Connected successfully to the DB server.");
+        const db = client.db(dbName);
+        
+        documents["_id"] = ObjectID;        
+	documents["UserName"] = req.body.UserName;	
+	documents['Date']= req.body.date;
+	documents['Borrow or Return']= req.body.borrow_or_return;
+	//documents['Book Type']= req.body.book_type;
+	//documents['Book Name']= req.body.book_name;
+        documents['Telephone Number']= req.body.phone_num;
+        documents['remark']= req.body.remark;
+        
+        var bookinfo ={};
+        bookinfo['Book Type'] = req.body.book_type;
+        if(req.body.book_name){
+            bookinfo['Book Name'] = req.body.book_name;
+        }
+        documents['Book Information']= bookinfo;
+        
+        console.log("...putting data into documents");
+        
+        documents["ownerID"] = `${req.session.userid}`;
+        
+     if(documents.UserName){
+        console.log("...Creating the document");
+        createDocument(db, documents, function(docs){
+            client.close();
+            console.log("Closed DB connection");
+            return res.status(200).render('info', {message: "Document is created successfully!"});
+        });
+    } else{
+        client.close();
+        console.log("Closed DB connection");
+        return res.status(200).render('info', {message: "Invalid entry - User Name is compulsory!"});
+    }
+        
+        
+    });
+    
 });
 
 
